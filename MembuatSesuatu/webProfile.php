c9d3a791a8f746c88e38c7cd860212157c6a54da
msynn <fyanramadhan1@gmail.com>
2022-03-04 23:53:41 +0930

    bkin web

---
+++ MembuatSesuatu/webProfile.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <section>
+        <!-- Navigation -->
+        <nav>
+            <!-- logo -->
+            <a href="#" class="logo">M S Y N</a>
+            <!-- menu -->
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Team</a></li>
+            </ul>
+        </nav>
+            <!-- text -->
+            <div class="text-conatiner">
+            <p>Hello,</p>
+            <p>I'M FYAN RAMADHAN</p>
+            <p>Bingung mau tulis apa HEHE</p>
+            <button class="hire-btn">JOKI MSYN</button>
+            <button class="down-cv">My Achivement</button>
+        </div>
+        <!-- Model -->
+         
+    </section>
+</body>
+</html>
